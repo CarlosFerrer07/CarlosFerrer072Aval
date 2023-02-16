db3ca349e71e938eb4856ab4048ef141dc91ca85
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-02-16 09:38:40 +0100

    añadimos el bminymax.sh

--- bminymax.sh
+++ bminymax.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+#b.- Vuelve a la rama parte1. Haz un script que se llame bminymax.sh, que me devuelva la ciudad, el mes y el año de consumo máximo y mínimo. Comitea, integra en master y sube a GitHub (1 punto)
+
+
+maxValencia=`cat consumos.txt | grep Valencia | head -1 | tail -1`
+minValencia=`cat consumos.txt | grep Valencia | sort -r | head -1 | tail -1`
+maxBarcelona=`cat consumos.txt | grep Barcelona | head -1 | tail -1`
+minBarcelona=`cat consumos.txt | grep Barcelona | sort -r | head -1 | tail -1`
+maxMadrid=`cat consumos.txt | grep Madrid | head -1 | tail -1`
+minMadrid=`cat consumos.txt | grep Madrid | sort -r | head -1 | tail -1`
+echo "CONSUMO MÁXIMO $maxValencia"
+echo "CONSUMO MÍNIMO $minValencia"    
+echo "CONSUMO MÁXIMO $maxBarcelona"
+echo "CONSUMO MÍNIMO $minBarcelona"   
+echo "CONSUMO MÁXIMO $maxMadrid"
+echo "CONSUMO MÍNIMO $minMadrid"   
+
+
+
