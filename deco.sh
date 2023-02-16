3894429e5485c71d4126eefb92d2d756688e80e7
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-02-16 09:59:46 +0100

    terminamos el del eco

--- deco.sh
+++ deco.sh
@@ -1,18 +1,6 @@
 #d.- Vuelve a la rama parte1. Haz un script que se llame deco.sh, que pasándole por parámetro una ciudad me devuelva la calificación eco atendiendo a: Si la media es inferior a 400 será ECO si es igual o superior será NO ECO. Utiliza el sript anterior. Comitea, integra en master y sube a GitHub (1 punto
 
-consumo=0
-contador=0
-
-while read linea; do
-    ciudad=$(echo $linea | awk '{print $1}')
-    if [ $ciudad == $1 ]; then
-        consumoCiudad=$(echo $linea | awk '{print $4}')
-        consumo=$(($consumo + $consumoCiudad))
-        contador=$(($contador + 1))
-    fi
-done < consumos.txt
-
-media=$((consumo / contador))
+./cmedia.sh
 
 if [ $media -lt 400 ];then
     echo "ECO"
